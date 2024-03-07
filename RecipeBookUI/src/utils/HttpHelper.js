b05b2e5152872d362050dabc9d787e1d633301df
YogiBearH <YogiBearH@github.com>
2024-03-06 23:43:39 -0700

    Added Utils, HttpHelper, Constants

---
+++ RecipeBookUI/src/utils/HttpHelper.js
@@ -0,0 +1,17 @@
+import Constants from './constants';
+
+/**
+ * @name HttpHelper
+ * @description - Utility method for using fetch in a convenient manner
+ * @param {string} route address to ping
+ * @param {string} method http method
+ * @param {Object} payload object to send
+ * @return {Promise} - Promise from the fetch call
+ */
+export default (route, method, payload) => fetch(Constants.BASE_URL_API + route, {
+  method,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(payload)
+});
