9352423290357f9ce744e7d78aab447878e16022
YogiBearH <YogiBearH@github.com>
2024-03-15 23:35:11 -0600

    Created recipe card layout

--- RecipeBookUI/src/components/pages/Recipes/RecipePage.js
+++ RecipeBookUI/src/components/pages/Recipes/RecipePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styles from './RecipePage.module.css';
 import Constants from '../../common/Utils/constants.js';
+import { Grid } from '@mui/material';
+import RecipeCard from '../../common/Recipe-Card/RecipeCard.js';
 
 const RecipePage = () => {
     const [recipes, setRecipes] = useState([]);
@@ -9,4 +11,10 @@ const RecipePage = () => {
     useEffect(() => {
         fetchRecipes(setRecipes, setApiError);
     });
+
+    return (
+        <Grid item xs={8} sx={{marginLeft: '320px'}}>
+            <RecipeCard />
+        </Grid>
+    )
 }
